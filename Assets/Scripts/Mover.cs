8a5848dd292680c56295cd6cbacbd932a4cc923c
percipient24 <jcpigm@rit.edu>
2018-03-27 19:17:13 -0400

    Just transferring. Not fixed yet!

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : MonoBehaviour {
+
+	protected Vector3 position;
+	protected Vector3 acceleration;
+	protected Vector3 velocity;
+	protected Vector3 heading;
+
+	public float mass;
+	public float maxSpeed;
+	public float maxTurn;
+	public float radius;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		acceleration = Vector3.zero;
+		velocity = Vector3.zero;
+		heading = transform.forward;
+	}
+
+	protected abstract void CalcSteeringForces ();
+
+	protected void ApplyForce(Vector3 force) {
+		acceleration += force / mass;
+	}
+
+	void UpdatePosition() {
+		velocity += acceleration;
+		if (velocity.sqrMagnitude > maxSpeed * maxSpeed) {
+			velocity = velocity.normalized * maxSpeed;
+		}
+
+		position += velocity * Time.deltaTime;
+
+		heading = velocity.normalized;
+
+		acceleration = Vector3.zero;
+	}
+
+	void SetTransform() {
+		float angle = Mathf.Atan2 (heading.y, heading.x);
+		Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.up);
+
+		this.transform.SetPositionAndRotation (position, rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CalcSteeringForces ();
+		UpdatePosition ();
+		SetTransform ();
+	}
+
+	public Vector3 Seek(Vector3 target) {
+		Vector3 desiredVelocity = target - position;
+		desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+		return desiredVelocity;
+	}
+}
---
+++ Assets/Scripts/Seeker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeker : Mover {
+
+	public GameObject target;
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+		mass = 3f;
+		maxSpeed = 1f;
+		maxTurn = 0.25f;
+		radius = 0.5f;
+	}
+
+	protected override void CalcSteeringForces ()
+	{
+		Vector3 steerForce = Vector3.zero;
+
+		Vector3 seekForce = Seek (target.transform.position);
+
+		steerForce += seekForce;
+
+		if (steerForce.sqrMagnitude > maxTurn * maxTurn) {
+			steerForce = steerForce.normalized * maxTurn;
+		}
+
+		ApplyForce (seekForce);
+	}
+}
